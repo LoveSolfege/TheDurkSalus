6ffd93fa46fbc295a2e9742e434cb20bf143de46
lovesolfege <chixran@gmail.com>
2024-11-04 01:17:13 +0400

    a little rebuild for enemy army creator class

--- Controllers/EnemyArmyCreator.cs
+++ Controllers/EnemyArmyCreator.cs
@@ -10,28 +10,25 @@ public static class EnemyArmyCreator
 	private static readonly IPlayer Player = new ComputerPlayer();
 	private static Team _firstEncounter = new Team(Player);
 	private static Team _secondEncounter = new Team(Player);
+	private static Team _thirdEncounter = new Team(Player);
 	
 	
 	
 	public static List<Team> CreateArmy()
 	{
-		_firstEncounter.AddEnemy(1);
-		_secondEncounter.AddEnemy(2);
-		List<Team> army = [_firstEncounter, _secondEncounter];
+		_firstEncounter.AddEnemy<Skeleton>(1);
+		_secondEncounter.AddEnemy<Skeleton>(2);
+		_thirdEncounter.AddEnemy<FinalBoss>(1);
+		List<Team> army = [_firstEncounter, _secondEncounter, _thirdEncounter];
 		return army;
 	}
 
 
-	private static void AddEnemy(this Team team, int amount)
-	{
-		BulkAddEnemy(team, amount);
-	}
-	
-	private static void BulkAddEnemy(Team team, int amount)
+	private static void AddEnemy<TEnemy>(this Team team, int amount) where TEnemy : Creature, new()
 	{
 		for (int i = 0; i < amount; i++)
 		{
-			team.AddMember(AddEnemy<Skeleton>());
+			team.AddMember(AddEnemy<TEnemy>());
 		}
 	}
 	
