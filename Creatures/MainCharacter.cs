f4b7d585d1554dc8c34bbe4978ab4b5a256567cb
LoveSolfege <chixran@gmail.com>
2024-11-05 05:57:40 +0400

    switched to primary constructor

--- Creatures/MainCharacter.cs
+++ Creatures/MainCharacter.cs
@@ -5,15 +5,10 @@ using TheDurkSalus.Models;
 
 namespace TheDurkSalus.Creatures;
 
-public class MainCharacter : Creature
+public class MainCharacter(string? name) : Creature
 {
-	public override string? Name { get; }
+	public override string? Name { get; } = name;
 	public override IAttack StandardAttack => new Punch();
 	public override double MaxHp => HealthPoints.PlayerMaxHp;
 	public override double CurrentHp { get; protected set; } = HealthPoints.PlayerMaxHp;
-
-	public MainCharacter(string? name) : base()
-	{
-		Name = name;
-	}
 }
